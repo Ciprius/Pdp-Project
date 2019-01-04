e9f47b0f5af4adf2a0ca2e81278a28aa8bac49be
Cipri Cuibus <ciprian.cuibus1@gmail.com>
2019-01-04 17:07:59 +0200

    Thread Implementation

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.Controller;
 
 namespace Project
 {
@@ -10,6 +11,30 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("1: continue.");
+                Console.WriteLine("0: exit.");
+                Console.Write("Select one option: ");
+                string op = Console.ReadLine();
+
+                if (op.Equals("0"))
+                    break;
+                if (op.Equals("1"))
+                {
+                    Console.Write("Give the number of vertices:");
+                    int nrV = Convert.ToInt32(Console.ReadLine());
+
+                    Cont cont = new Cont(nrV);
+                    cont.InitMatrix(nrV);
+                    cont.Generate(nrV);
+                    cont.Start();
+                    foreach (var thread in cont.GetThreads())
+                        thread.Join();
+
+                    Console.WriteLine(cont.Show());
+                }
+            }
         }
     }
 }
